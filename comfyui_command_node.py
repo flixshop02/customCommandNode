4cde39495ef1c1d78efe3856e81b0f9ca3697d90
flixshop02 <digitalflixshop02@gmail.com>
2025-04-24 20:36:39 +0600

    ff

--- comfyui_command_node.py
+++ comfyui_command_node.py
@@ -1,24 +1,20 @@
-class CommandLineNode:
+from nodes import ComfyNode, NODE_CLASS_MGR
+
+class CommandLineNode(ComfyNode):
     @classmethod
     def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "command": ("STRING", {"multiline": False, "default": ""}),
-            }
-        }
+        return {"required": {"command": ("STRING", {"multiline": False, "default": ""})}}
+
     RETURN_TYPES = ("STRING",)
     RETURN_NAMES = ("output",)
     FUNCTION = "run_command"
     CATEGORY = "Custom"
 
+    @classmethod
+    def NAME(cls):
+        return "Run Command"
+
     def run_command(self, command):
         return (f"You entered: {command}",)
 
-NODE_CLASS_MGR = None
-try:
-    from . import NODE_CLASS_MGR
-except ImportError:
-    pass
-
-if NODE_CLASS_MGR:
-    NODE_CLASS_MGR.register_node(CommandLineNode)
+NODE_CLASS_MGR.register_node(CommandLineNode)
