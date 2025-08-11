caff52d69661144d00539d9fa9b04157ebd794e2
tanmoy-it <tanmoy.it@palmalgarments.com>
2025-08-11 10:59:14 +0600

    nope, no code mirror

--- __init__.py
+++ __init__.py
@@ -3,8 +3,6 @@
 # Make sure the filename here matches the file containing your node class.
 from .command_executor import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-WEB_DIRECTORY = "./js"  # Tell ComfyUI to load frontend assets from the js directory
-
 # Export the mappings for ComfyUI to discover the nodes
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
